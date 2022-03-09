455624731b1222bbc9e88a1ee264d5af700953a2
Nguyen Tri Kien <kienpmh@gmail.com>
2022-03-09 22:57:33 +0700

    back-end (Register and login)

---
+++ Group11_COMP1640_backend/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Group11_COMP1640_backend.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public String FirstName { get; set; }
+    public String LastName { get; set; }
+
+    public String Email { get; set; }
+}
+
