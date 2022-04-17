b85cba3fa256306a4a08da08a9e22da92c8f8de0
Hoang Quan <quannguyen5065@gmail.com>
2022-04-17 16:25:19 +0700

    delete to reup sorce

--- Group11_COMP1640_backend/Core/Repositories/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASP.NETCoreIdentityCustom.Core.Repositories
-{
-    public interface IUnitOfWork
-    {
-        IUserRepository User { get; }
-
-        IRoleRepository Role { get; }
-    }
-}
